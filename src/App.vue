251c0469e83371e9cb7f76786063205e1d1f15c3
maki9991 <tdm_yamada@cc.nara-wu.ac.jp>
2021-08-07 15:07:23 +0900

    Human Clothes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="app">
-    <simple-memo />
+  <div class="container">
+    <Human>👚</Human>
+    <!--3. 普通のHTMLタグのようにHumanコンポーネントを書く-->
+    <Human>👘 </Human>
+    <Human>👗</Human>
   </div>
 </template>
 
 <script>
-import SimpleMemo from "./components/SimpleMemo.vue";
+import Human from "@/components/Human.vue"; //1. HumanコンポーネントをHuman.vueから読み込む
+
+// @はsrcディレクトリを指すよ！（絶対パス）
 export default {
-  components: { SimpleMemo },
+  components: {
+    Human, //2. HumanコンポーネントをApp.vueのなかで使えるように登録する
+  },
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+</style>
